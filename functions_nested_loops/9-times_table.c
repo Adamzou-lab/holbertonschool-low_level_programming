175677864d27b7753075a86c991b254a490ef94d
Adamzou-lab <adampro@icloud.com>
2026-02-24 16:10:59 +0100

    feat: add times_table function to print the 9 times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int ligne;
+	int colonne;
+	int resultat = 0;
+
+	for (ligne = 0; ligne <= 9; ligne++)
+	{
+		for (colonne = 0; colonne <= 9; colonne++)
+		{
+			resultat = ligne * colonne;
+
+			if (colonne != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (resultat < 10)
+			{
+				_putchar(' ');
+			}
+
+			if (resultat < 10)
+			{
+				_putchar((resultat % 10) + '0');
+			}
+			else
+			{
+				_putchar((resultat / 10) + '0');
+				_putchar((resultat % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
