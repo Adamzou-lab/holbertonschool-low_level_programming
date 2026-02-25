d97ad273b9b928f0aafb47f389140dd1c30a597c
Adamzou-lab <adampro@icloud.com>
2026-02-25 08:34:17 +0100

    feat: implement add function to perform addition of two integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @i: first integer to be added
+ * @j: second integer to be added
+ *
+ * Return: the result of the addition
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
