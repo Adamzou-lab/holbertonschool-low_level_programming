ebf299bda4654ebe9522219620732d49bf3f1938
Adamzou-lab <adampro@icloud.com>
2026-02-25 14:13:07 +0100

    feat: add multiplication function and main test file

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplie deux entiers
+ * @a: le premier entier
+ * @b: le deuxième entier
+ *
+ * Return: le résultat de la multiplication de a et b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
