477cf70737edd54dbf8e570f8314fc7f36b327b0
Adamzou-lab <adampro@icloud.com>
2026-02-25 14:09:53 +0100

    feat: add _isdigit function to check if a character is a digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - vérifie si un caractère est un chiffre (0 à 9)
+ * @c: le caractère à vérifier
+ *
+ * Return: 1 si c est un chiffre, 0 sinon
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else if (c >= 'a' && c <= 'z')
+	{
+		return (0);
+	}
+	return (0);
+}
