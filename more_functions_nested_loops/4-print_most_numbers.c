d72b31a1d27c093b441f5001f9759a607fec9fd6
Adamzou-lab <adampro@icloud.com>
2026-02-25 14:27:54 +0100

    feat: add print_most_numbers function to display digits from 0 to 9 excluding 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers - affiche les chiffres de 0 à 9
+ * sauf 2 et 4 suivis d'une nouvelle ligne
+ *
+ * Return: void (pas de valeur de retour)
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+		i++;
+	}
+	_putchar('\n');
+}
