f82554764952f49df33ba07c587eb1b43f912a35
Adamzou-lab <adampro@icloud.com>
2026-03-02 15:43:35 +0100

    feat: add swap_int function and main program for testing feat: implement _strlen function and main program for testing

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * swap_int - Swaps the values of two integers.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
+ *
+ * Description: Takes two pointers to int as parameters and
+ * swaps the values they point to.
+ *
+ * Return: Nothing.
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
