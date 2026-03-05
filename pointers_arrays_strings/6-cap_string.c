62d2308b61fb38214365274c0e0b6f688be4365c
Adamzou-lab <adampro@icloud.com>
2026-03-05 13:32:00 +0100

    fix: update parameter name in cap_string function for consistency

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * cap_string - Capitalizes all words of a string.
- * @str: The string to be modified.
+ * @s: The string to be modified.
  *
  * Description: Scans the string and capitalizes the first letter
  * of every word. Words are delimited by a specific set of separators:
@@ -9,30 +9,28 @@
  *
  * Return: Pointer to the modified string s.
  */
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
 	int i = 0;
-	int j;
 	int separateur = 1;
 	char *sep = " \t\n,;.!?\"(){}";
 
-	while (str[i] != '\0')
+	while (s[i] != '\0')
 	{
-		j = 0;
-		while (sep[j] != '\0' && str[i] != sep[j])
+		int j = 0;
+
+		while (sep[j] != '\0' && s[i] != sep[j])
 			j++;
 		if (sep[j] != '\0')
 			separateur = 1;
-		else if (str[i] >= 'a' && str[i] <= 'z' && separateur == 1)
+		else if (s[i] >= 'a' && s[i] <= 'z' && separateur == 1)
 		{
-			str[i] -= 32;
+			s[i] -= 32;
 			separateur = 0;
 		}
-		else if (str[i] >= 'A' && str[i] <= 'Z' && separateur == 0)
-			str[i] += 32;
 		else
 			separateur = 0;
 		i++;
 	}
-	return (str);
+	return (s);
 }
