8f74d7c2b04cd1284782d3fd0f20088ca3c728d7
Adamzou-lab <adampro@icloud.com>
2026-03-05 12:58:51 +0100

    refactor: simplify cap_string by removing grand_separateur function and using inline separator checks

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,56 +1,37 @@
 #include "main.h"
 /**
- * grand_separateur - Checks if a character is a word separator.
- * @c: The character to check.
- *
- * Return: 1 if it is a separator, 0 otherwise.
- */
-int grand_separateur(char c)
-{
-	if (c == ' ' || c == '\t' || c == '\n' || c == ','
-		|| c == ';' || c == '.' || c == '!' || c == '?'
-		|| c == '"' || c == '(' || c == ')' || c == '{'
-		|| c == '}')
-		return (1);
-	return (0);
-}
-/**
  * cap_string - Capitalizes all words of a string.
  * @s: The string to be modified.
  *
- * Description: Capitalizes the first letter of every word based on
- * specific separators and ensures other letters are lowercase.
+ * Description: Scans the string and capitalizes the first letter
+ * of every word. Words are delimited by a specific set of separators:
+ * space, tabulation, new line, ,, ;, ., !, ?, ", (, ), {, and }.
  *
  * Return: Pointer to the modified string s.
  */
 char *cap_string(char *s)
 {
 	int i = 0;
+	int j;
 	int separateur = 1;
+	char *sep = " \t\n,;.!?\"(){}";
 
 	while (s[i] != '\0')
 	{
-		if (grand_separateur(s[i]))
-		{
+		j = 0;
+		while (sep[j] != '\0' && s[i] != sep[j])
+			j++;
+		if (sep[j] != '\0')
 			separateur = 1;
-		}
 		else if (s[i] >= 'a' && s[i] <= 'z' && separateur == 1)
 		{
 			s[i] -= 32;
 			separateur = 0;
 		}
 		else if (s[i] >= 'A' && s[i] <= 'Z' && separateur == 0)
-		{
 			s[i] += 32;
-		}
-		else if (s[i] >= 'A' && s[i] <= 'Z' && separateur == 1)
-		{
-			separateur = 0;
-		}
 		else
-		{
 			separateur = 0;
-		}
 		i++;
 	}
 	return (s);
