dbdeadff90544bf5d782c0999d3dfcfeb22b6e0d
Adamzou-lab <adampro@icloud.com>
2026-03-11 18:55:43 +0100

    feat: implement print_name function and header file for function pointers

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - Prints a name using a given function.
+ * @name: Name to be printed.
+ * @f: Pointer to the function to use for printing.
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
