38157380fe7c731908a687e33ca95bb5cd8b7ad5
Adamzou-lab <adampro@icloud.com>
2026-03-15 15:28:03 +0100

    fix: correct header file inclusion and improve function structure in get_op_func

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,5 +1,8 @@
-#include "calc.h"
+#include "3-calc.h"
+#include <stdio.h>
 
+int (*get_op_func(char *s))(int, int)
+{
  op_t ops[] = {
         {"+", op_add},
         {"-", op_sub},
@@ -8,5 +11,13 @@
         {"%", op_mod},
         {NULL, NULL}
     };
-    int i;
-	
+    int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (*(ops[i].op) == *s && s[1] == '\0')
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
