73b42ea27d76166b2faddc755e8b80aa97186780
Adamzou-lab <adampro@icloud.com>
2026-03-15 15:33:47 +0100

    fix: improve documentation and formatting in calculator functions

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,6 +1,13 @@
 #include "3-calc.h"
 #include <stdio.h>
-
+/**
+ * get_op_func - Selects the correct function to perform the operation.
+ * @s: The operator passed as argument to the program.
+ *
+ * Return: A pointer to the function that corresponds to the operator
+ * given as a parameter. If s does not match any of the 5 expected
+ * operators, return NULL.
+ */
 int (*get_op_func(char *s))(int, int)
 {
  op_t ops[] = {
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,7 +1,13 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - Effectue des opérations simples.
+ * @argc: Nombre d'arguments.
+ * @argv: Tableau des arguments.
+ *
+ * Return: 0 en cas de succès.
+ */
 int main(int argc, char *argv[])
 {
 	int num1;
