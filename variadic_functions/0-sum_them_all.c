04de5042ffab46d4cca7818a17982257137a9f53
Adamzou-lab <adampro@icloud.com>
2026-03-16 14:45:53 +0100

    fix: reorder includes and simplify loop in sum_them_all function

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,5 +1,5 @@
-#include <stdarg.h>
 #include "variadic_functions.h"
+#include <stdarg.h>
 /**
  * sum_them_all - returns the sum of all its parameters
  * @n: the number of parameters to sum
@@ -11,18 +11,15 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
-	unsigned int i = 0;
-	int sum = 0;
+	unsigned int i;
+	unsigned int sum = 0;
 
 	if (n == 0)
 		return (0);
 
 	va_start(args, n);
-	while (i < n)
-	{
+	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
-		i++;
-	}
 	va_end(args);
 	return (sum);
 }
