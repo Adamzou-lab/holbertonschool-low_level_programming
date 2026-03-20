6cbe09ae765c1c31ad0bf9b61e62dad4fed2b81c
Adamzou-lab <adampro@icloud.com>
2026-03-20 22:05:13 +0100

    fix: changing the if

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,17 +1,14 @@
 #include "function_pointers.h"
-#include <stdio.h>
 /**
- * print_name - Prints a name using a given function.
- * @name: Name to be printed.
- * @f: Pointer to the function to use for printing.
+ * print_name - prints a name using a function pointer
+ * @name: the name to print
+ * @f: pointer to the function to use
  *
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL)
-		return;
-	if (f == NULL)
+	if (name == NULL || f == NULL)
 		return;
 	f(name);
 }
