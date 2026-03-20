e023cf966ed34a1dfc9c99e9c953388eb47e3acc
Adamzou-lab <adampro@icloud.com>
2026-03-20 22:01:51 +0100

    fix: recoding the whole files

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,53 +1,35 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * string_nconcat - Concatenates two strings up to n bytes of s2.
- * @s1: The first string.
- * @s2: The second string.
- * @n: The maximum number of bytes of s2 to concatenate.
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes from s2 to append
  *
- * Description: If n is greater or equal to the length of s2,
- * then the entire string s2 is used. If NULL is passed,
- * it is treated as an empty string.
- *
- * Return: Pointer to the newly allocated space in memory,
- * or NULL if the function fails.
+ * Return: pointer to newly allocated string, or NULL on failure
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0;
-	unsigned int j = 0;
-	unsigned int bytes;
-	char *tab;
+	char *result;
+	unsigned int len1, len2, i;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[i])
-		i++;
-	while (s2[j])
-		j++;
-	if (n >= j)
-		bytes = j;
-	else
-		bytes = n;
-	tab = malloc(i + bytes + 1);
-	if (tab == NULL)
-		tab = (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		tab[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (j < bytes)
-	{
-		tab[i] = s2[j];
-		j++;
-		i++;
-	}
-	tab[i] = '\0';
-	return (tab);
+	for (len1 = 0; s1[len1]; len1++)
+		;
+	for (len2 = 0; s2[len2]; len2++)
+		;
+	if (n >= len2)
+		n = len2;
+	result = malloc(len1 + n + 1);
+	if (result == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+	for (i = 0; i < n; i++)
+		result[len1 + i] = s2[i];
+	result[len1 + n] = '\0';
+	return (result);
 }
