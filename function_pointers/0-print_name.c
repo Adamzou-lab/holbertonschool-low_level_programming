d45c144a5f3fb9b8aefc9a35fbe5f2e6909d44a3
Adamzou-lab <adampro@icloud.com>
2026-03-20 22:16:08 +0100

    fix: fix for requirements

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,6 +1,7 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
- * print_name - prints a name using a function pointer
+ * print_name - prints a name using a given function
  * @name: the name to print
  * @f: pointer to the function to use
  *
