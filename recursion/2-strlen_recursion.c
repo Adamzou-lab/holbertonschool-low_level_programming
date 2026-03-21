04992ed2a7ac8be025f6450d6f4342bd6c81f6ba
Adamzou-lab <adampro@icloud.com>
2026-03-21 19:11:35 +0100

    fix: delete trailing whitespace

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -3,7 +3,7 @@
 /**
  * _strlen_recursion - Count the lenght of a string
  * @s: The string to count
- * 
+ *
  * Return: The lenght of the string or NULL if s is 0
  */
 int _strlen_recursion(char *s)
