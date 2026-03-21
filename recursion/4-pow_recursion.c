b73c51857668b2b844336869b307891e7957cd25
Adamzou-lab <adampro@icloud.com>
2026-03-21 19:51:36 +0100

    feat: add _pow_recursion function

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion - Computes x raised to the power of y
+ * @x: First integer
+ * @y: Second integer
+ * 
+ * Return: The value of x raised to the power of y, or -1 if y is negative
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
