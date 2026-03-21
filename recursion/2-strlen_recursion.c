d03cf7d8fe2ead966af494c071c8cc8f905ecba2
Adamzou-lab <adampro@icloud.com>
2026-03-21 19:10:27 +0100

    feat: add _strlen_recursion that count the lenght of a string

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen_recursion - Count the lenght of a string
+ * @s: The string to count
+ * 
+ * Return: The lenght of the string or NULL if s is 0
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
