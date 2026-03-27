b291fcd7f9bae85b09c4cc1d14190d9ef77beef8
Adamzou-lab <adampro@icloud.com>
2026-03-27 22:39:44 +0100

    fix: betty description

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,5 +1,11 @@
 #include "lists.h"
-
+/**
+ * add_node - Adds a new node at the beginning of a list_t list.
+ * @head: A double pointer to the list_t list.
+ * @str: The string to be added to the new node.
+ *
+ * Return: The address of the new element, or NULL if it failed.
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
