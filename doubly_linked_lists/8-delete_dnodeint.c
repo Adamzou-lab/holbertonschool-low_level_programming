ffacbf1225b39c95be3562af485a00d1bc4c45e7
Adamzou-lab <adampro@icloud.com>
2026-04-02 16:33:16 +0200

    feat: add delete_dnodeint_at_index function

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - deletes the node at index of a dlistint_t list
+ * @head: pointer to the pointer of the head of the list
+ * @index: index of the node to delete, starting from 0
+ *
+ * Return: 1 if succeeded, -1 if failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current;
+	unsigned int count;
+
+	if (*head == NULL)
+    	return (-1);
+
+	current = *head;
+	count = 0;
+
+	if (index == 0)
+	{
+		*head = current->next;
+		if (*head != NULL)        /* vérifier avant d'accéder à ->prev */
+        	(*head)->prev = NULL;
+		free(current);
+		return (1);
+	}
+	count = 1;
+	while (current != NULL)
+	{
+		if (count == index)
+		{
+			if (current->next == NULL)
+            {
+				current->prev->next = NULL;
+				free(current);
+                return (1);
+            }
+
+			current->prev->next = current->next;
+			current->next->prev = current->prev;
+			free(current);
+			return (1);
+		}
+		count++;
+		current = current->next;
+	}
+	return (-1); /* index > taille */
+}
