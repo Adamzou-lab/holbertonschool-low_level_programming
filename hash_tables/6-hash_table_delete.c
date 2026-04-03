3f83bd5315ad6d4363bf349eeb55b77ee628e938
Adamzou-lab <adampro@icloud.com>
2026-04-03 18:24:23 +0200

    feat: add hash_table_delete function

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: The hash table to delete
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *node;
+	hash_node_t *next;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			next = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = next;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
